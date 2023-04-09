d1e06eea9a1c537d04e1d362d1c8a665a32ddb2e
Tema Mint <37416188+temamint@users.noreply.github.com>
2023-04-09 16:49:35 +0400

    Created and filled store.ts (#6)
    Co-authored-by: ance1 <37416188+BlagoTema@users.noreply.github.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,43 +1,76 @@
 import { InjectionKey } from 'vue'
 import { createStore, Store } from 'vuex'
 
+// assets
+import BanksIcon1 from '../assets/banks/banksIcon_1.svg';
+import BanksIcon2 from '../assets/banks/banksIcon_2.svg';
+import BanksIcon3 from '../assets/banks/banksIcon_3.svg';
+import BanksIcon4 from '../assets/banks/banksIcon_4.svg';
+import BanksIcon5 from '../assets/banks/banksIcon_5.svg';
+
 // interfaces
+export interface IStore {
+	paymentInfo: IPaymentInfo;
+	banks: IBanks[];
+}
+
+export interface IPaymentInfo {
+	requestId: number;
+	summ: number;
+	pickedBank: IBanks | null;
+	card: number;
+	comment: string | null;
+	status: 'inProgress' | false | true;
+}
+
 export interface IBanks {
-	banks: {
-		logo: string,
-		name: string
-	}[];
+	logo: string;
+	name: string;
 }
 
 
 // key
-export const key: InjectionKey<Store<IBanks>> = Symbol()
+export const key: InjectionKey<Store<IStore>> = Symbol()
 
 
 // data
-export const store = createStore<IBanks>({
+export const store = createStore<IStore>({
 	state: {
+		paymentInfo: {
+			requestId: 56712,
+			summ: 500,
+			pickedBank: null,
+			card: 1234567812345678,
+			comment: null,
+			status: 'inProgress'
+		},
+
 		banks: [
 			{
-				logo: 'image',
+				logo: BanksIcon1,
 				name: 'А-банк',
 			},
 			{
-				logo: 'image',
+				logo: BanksIcon2,
 				name: 'Izibank',
 			},
 			{
-				logo: 'image',
+				logo: BanksIcon3,
 				name: 'Monobank',
 			},
 			{
-				logo: 'image',
+				logo: BanksIcon4,
 				name: 'ПриватБанк',
 			},
 			{
-				logo: 'image',
+				logo: BanksIcon5,
 				name: 'ПУМБ',
 			},
 		],
 	},
+	mutations: {
+		userPickBank (state, payload) {
+			state.paymentInfo.pickedBank = payload.bank;
+		}
+	  }
 });
