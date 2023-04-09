e44ab437fdf085d0b27774b778fcc085b2c5e72b
Tema Mint <37416188+temamint@users.noreply.github.com>
2023-04-09 17:39:13 +0400

    Created 'Transfer' component (#10)
    Co-authored-by: ance1 <37416188+BlagoTema@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,36 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import { store, key } from './store/store'
+
 import './style.css'
+
+// components
 import App from './App.vue'
+import PaymentMethod from './components/PaymentMethod.vue';
+import Transfer from './components/Transfer.vue';
+import PaymentRequest from './components/PaymentRequest.vue'
+
+const router = createRouter({
+	routes: [
+		{
+			path: '/',
+			component: PaymentMethod,
+		},
+		{
+			path: '/transfer',
+			component: Transfer,
+		},
+		{
+			path: '/request',
+			component: PaymentRequest,
+		},
+	],
+	history: createWebHistory(),
+});
 
 const app = createApp(App);
 
+app.use(router);
 app.use(store, key);
 
 app.mount('#app');
